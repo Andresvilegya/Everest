44a39d63fd81462350764d772478927667adedd4
Andrey Lysenko <Andresvileg@yandex.ru>
2025-08-01 17:54:12 +0300

    Update contact_id_x_legal_verification_id.sql

--- contact_id_x_legal_verification_id.sql
+++ contact_id_x_legal_verification_id.sql
@@ -1,14 +1,14 @@
--- mbaf_system.dbo.contact_id_x_legal_verification_id определение
+-- mbaf_system.dbo.contact_emails_in_x_legal определение
 
 -- Drop table
 
--- DROP TABLE mbaf_system.dbo.contact_id_x_legal_verification_id;
+-- DROP TABLE mbaf_system.dbo.contact_emails_in_x_legal;
 
-CREATE TABLE mbaf_system.dbo.contact_id_x_legal_verification_id (
-	contact_id_x_legal_verification_id int IDENTITY(1,1) NOT NULL,
+CREATE TABLE mbaf_system.dbo.contact_emails_in_x_legal (
+	contact_emails_in_x_legal_id int IDENTITY(1,1) NOT NULL,
 	contact_id int NULL,
 	emails_in_id int NULL,
-	legal_verification_id int NOT NULL,
+	legal_id int NOT NULL,
 	insert_user nvarchar(50) COLLATE SQL_Latin1_General_CP1251_CI_AS DEFAULT suser_sname() NOT NULL,
 	insert_date datetime DEFAULT getdate() NOT NULL,
 	update_user nvarchar(50) COLLATE SQL_Latin1_General_CP1251_CI_AS NULL,
@@ -16,7 +16,7 @@ CREATE TABLE mbaf_system.dbo.contact_id_x_legal_verification_id (
 	delete_user nvarchar(50) COLLATE SQL_Latin1_General_CP1251_CI_AS NULL,
 	delete_date datetime NULL,
 	delete_statute tinyint DEFAULT 0 NOT NULL,
-	CONSTRAINT PK_contact_id_x_legal_verification_id PRIMARY KEY (contact_id_x_legal_verification_id),
-	CONSTRAINT FK_contract_contact_id_x_legal_verification_id FOREIGN KEY (contact_id) REFERENCES mbaf_system.dbo.contact(contact_id),
-	CONSTRAINT FK_emails_in_contact_id_x_legal_verification_id FOREIGN KEY (emails_in_id) REFERENCES mbaf_system.dbo.emails_in(emails_in_id)
+	CONSTRAINT PK_contact_id_x_legal_id PRIMARY KEY (contact_emails_in_x_legal_id),
+	CONSTRAINT FK_contact_id_x_legal_id FOREIGN KEY (contact_id) REFERENCES mbaf_system.dbo.contact(contact_id),
+	CONSTRAINT FK_emails_in_id_x_legal_id FOREIGN KEY (emails_in_id) REFERENCES mbaf_system.dbo.emails_in(emails_in_id)
 );
